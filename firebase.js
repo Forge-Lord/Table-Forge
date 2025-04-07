7d82b0507589db0021b78d8c091609d0b2e03403
Forge-Lord <whitepeacok006@gmail.com>
2025-04-07 00:05:07 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzvVpMCdg3Y6i5vCGWarorcTmzBzjmPow",
+  authDomain: "tableforge-app.firebaseapp.com",
+  projectId: "tableforge-app",
+  storageBucket: "tableforge-app.appspot.com",
+  messagingSenderId: "708497363618",
+  appId: "1:708497363618:web:39da060b48681944923dfb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db, doc, getDoc, setDoc, updateDoc, onSnapshot };
