acfcaa6f68359b0aede7e58421a157e414b30d68
Forge-Lord <whitepeacok006@gmail.com>
2025-04-15 21:33:18 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,46 @@
-// firebase.js – Shared Firebase initialization
+// firebase.js – Shared Firebase Init for Table Forge
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
   getDatabase,
   ref,
   set,
+  update,
   get,
   child,
-  onValue,
-  update
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+  onValue
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzvVpMCdg3Y6i5vCGWarorcTmzBzjmPow",
   authDomain: "tableforge-app.firebaseapp.com",
   projectId: "tableforge-app",
-  databaseURL: "https://tableforge-app-default-rtdb.firebaseio.com"
+  databaseURL: "https://tableforge-app-default-rtdb.firebaseio.com",
+  storageBucket: "tableforge-app.appspot.com",
+  messagingSenderId: "708497363618",
+  appId: "1:708497363618:web:39da060b48681944923dfb"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
-export { db, ref, set, get, child, onValue, update };
+export {
+  db,
+  ref,
+  set,
+  update,
+  get,
+  child,
+  onValue,
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
+};
