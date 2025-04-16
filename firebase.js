84c5d4c0754db7d341567bda0f780568381068bd
Forge-Lord <whitepeacok006@gmail.com>
2025-04-15 21:02:49 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+// firebase.js – Shared Firebase initialization
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import {
   getDatabase,
   ref,
+  set,
+  get,
+  child,
   onValue,
   update
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzvVpMCdg3Y6i5vCGWarorcTmzBzjmPow",
   authDomain: "tableforge-app.firebaseapp.com",
   projectId: "tableforge-app",
-  databaseURL: "https://tableforge-app-default-rtdb.firebaseio.com",
-  storageBucket: "tableforge-app.appspot.com",
-  messagingSenderId: "708497363618",
-  appId: "1:708497363618:web:39da060b48681944923dfb"
+  databaseURL: "https://tableforge-app-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-export { db, ref, onValue, update };
+
+export { db, ref, set, get, child, onValue, update };
